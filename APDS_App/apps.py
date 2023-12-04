ba5c9439839347fda7ec088be8a980a3f1d9b413
Rohith Jatla <jatlar1@udayton.edu>
2023-12-04 16:45:23 -0500

    modified Dirs

--- APDS_App/apps.py
+++ APDS_App/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Sdp2AppConfig(AppConfig):
+class APDS_AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'APDS_App'
